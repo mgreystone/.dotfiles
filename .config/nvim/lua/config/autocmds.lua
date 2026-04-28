a2cb889712c741424b976d074108359788984b56
Markus Greystone <markus.greystone@gmail.com>
2026-04-28 19:02:22 -0400

    Colors for the colorless

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -7,3 +7,38 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 ---- lua/config/autocmds.lua
+
+-- Dim inactive nvim windows within a focused instance
+local dim = vim.api.nvim_create_augroup("DimInactiveWindows", { clear = true })
+
+vim.api.nvim_create_autocmd("WinLeave", {
+  group = dim,
+  callback = function()
+    vim.wo.winhighlight = "Normal:NormalNC"
+  end,
+})
+
+vim.api.nvim_create_autocmd("WinEnter", {
+  group = dim,
+  callback = function()
+    vim.wo.winhighlight = ""
+  end,
+})
+
+-- Dim entire nvim instance when tmux focus moves to another pane
+vim.api.nvim_create_autocmd("FocusLost", {
+  group = dim,
+  callback = function()
+    for _, win in ipairs(vim.api.nvim_list_wins()) do
+      vim.wo[win].winhighlight = "Normal:NormalNC"
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("FocusGained", {
+  group = dim,
+  callback = function()
+    -- Restore only the active window; others stay dim via WinLeave
+    vim.wo.winhighlight = ""
+  end,
+})
--- .config/nvim/lua/plugins/solarized.lua
+++ .config/nvim/lua/plugins/solarized.lua
@@ -20,5 +20,8 @@ return {
     vim.o.background = "dark"
     require("solarized").setup(opts)
     vim.cmd.colorscheme("solarized")
+    -- Match tmux inactive pane color so unfocused nvim windows dim consistently
+    vim.api.nvim_set_hl(0, "Normal",   { bg = "#001c24" })
+    vim.api.nvim_set_hl(0, "NormalNC", { bg = "#002b36" })
   end,
 }
